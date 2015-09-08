75c1f130c45f6612598e2538a3076db2d53efb29
Daniel Meyer <danielmeyer@danielrmeyer.emsi>
2015-09-08 16:14:21 -0700

    turned on debugging and added some urls

--- app/hello_world.py
+++ app/hello_world.py
@@ -2,9 +2,21 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return "Index Page"
 
+@app.route('/hello')
+def hello():
+    return "Hello World Page!"
+
+@app.route('/foo')
+def foo():
+    return "Foo page"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "User %s" % username
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host='0.0.0.0', port=80)
